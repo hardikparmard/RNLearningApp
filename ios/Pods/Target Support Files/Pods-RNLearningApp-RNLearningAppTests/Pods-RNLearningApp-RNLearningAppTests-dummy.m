8339234831e80775e30e74d5bf57d03fc4db2d60
Hardik Parmar <hardik.parmar@dream11.com>
2021-05-04 20:14:33 +0530

    updated gitignore and removed ignored files

--- ios/Pods/Target Support Files/Pods-RNLearningApp-RNLearningAppTests/Pods-RNLearningApp-RNLearningAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RNLearningApp_RNLearningAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_RNLearningApp_RNLearningAppTests
-@end
--- ios/Pods/Target Support Files/Pods-RNLearningApp/Pods-RNLearningApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RNLearningApp : NSObject
-@end
-@implementation PodsDummy_Pods_RNLearningApp
-@end
